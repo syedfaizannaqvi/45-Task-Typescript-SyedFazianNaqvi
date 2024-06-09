7ba3aa3124ec29c7f9ccfba3efe80fe7254c3f87
syedfaizannaqvi <164246862+syedfaizannaqvi@users.noreply.github.com>
2024-06-09 13:39:02 -0700

    Add files via upload

---
+++ Ex11.ts
@@ -0,0 +1,14 @@
+/*
+Names: Store the names of a few of your friends
+in a array called names. Print each person’s
+name by accessing each element in the list, 
+one at a time.
+*/
+const personName: string[] = ["Ali", "Fahad", "Muzamil", "Raqib", "Faiz"];
+
+console.log(personName[0]);
+console.log(personName[1]);
+console.log(personName[2]);
+console.log(personName[3]);
+
+
