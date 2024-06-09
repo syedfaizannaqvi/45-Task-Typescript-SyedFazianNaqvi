dff58684a2eb416d06b3c247d3f403df9c3dc120
syedfaizannaqvi <164246862+syedfaizannaqvi@users.noreply.github.com>
2024-06-09 13:36:55 -0700

    Add files via upload

---
+++ Ex9.ts
@@ -0,0 +1,11 @@
+/*
+Favorite Number: Store your favorite number in
+a variable. Then, using that variable, create
+a message that reveals your favorite number.
+Print that message.
+*/
+let favNumb: number = 8;
+let message: number = `${favNumb}`;
+console.log(message);
+
+
