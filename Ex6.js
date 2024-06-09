75577565dff42a5d949ee89dce07585ea3f4244a
syedfaizannaqvi <164246862+syedfaizannaqvi@users.noreply.github.com>
2024-06-09 13:46:39 -0700

    Add files via upload

---
+++ Ex6.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+Stripping Names: Store a person’s name, and
+include some whitespace characters at the
+beginning and end of the name. Make sure you
+use each character combination, "\t" and "\n",
+at least once. Print the name once, so the
+whitespace around the name is displayed. Then
+print the name after striping the white
+spaces.
+*/
+//const myName: string = "  Faizan  ";
+//const myName: string = "\tFaizan";
+const myName = "\nFaizan";
+console.log(myName);
