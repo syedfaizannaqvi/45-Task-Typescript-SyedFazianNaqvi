4bbed9733b7472915fe106de64b2ab4e07b03c04
syedfaizannaqvi <164246862+syedfaizannaqvi@users.noreply.github.com>
2024-06-09 13:39:54 -0700

    Add files via upload

---
+++ Ex12.ts
@@ -0,0 +1,16 @@
+/*
+Greetings: Start with the array you used in
+Exercise 11, but instead of just printing 
+each person’s name, print a message to them. 
+The text of each message should be the same, 
+but each message should be personalized with 
+the person’s name.
+*/
+const friendsName: string[] =["Fawad", "Faisal", "Faisal", "Razzaq", "Abdul"];
+
+console.log(`Hello ${friendsName[0]}, How are you?`);
+console.log(`Hello ${friendsName[1]}, How are you?`);
+console.log(`Hello ${friendsName[2]}, How are you?`);
+console.log(`Hello ${friendsName[3]}, How are you?`);
+console.log(`Hello ${friendsName[4]}, How are you?`);
+
