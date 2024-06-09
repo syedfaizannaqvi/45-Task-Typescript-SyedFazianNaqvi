4553e91da1872033017b45828ff5309c9c87ec4b
syedfaizannaqvi <164246862+syedfaizannaqvi@users.noreply.github.com>
2024-06-09 13:43:12 -0700

    Delete Task 2.ts

--- Task 2.ts
+++
@@ -1,6 +0,0 @@
-// Task 2 personal message: store a person's name in a variable, and print a message to that person. your message should be simple, such as "Hello Eric, would you like to learn
-// some python today?
-
-let person_name: string = "Eric";
-console.log(`Hello ${person_name}, would you like to learn some python today?);
-
